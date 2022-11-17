b4edd5ac039a4a509e85fa29f9560979d7b1b729
tihmihvik <tihmihvik@gmail.com>
2022-11-17 22:10:33 +0700

    Refactored to Generic class

--- GenericTypesLeaming/GenericTypesLeaming/src/customgenerictypes/Account.java
+++ GenericTypesLeaming/GenericTypesLeaming/src/customgenerictypes/Account.java
@@ -1,19 +1,19 @@
 package customgenerictypes;
 
-public class Account {
-    private Object id;
+public class Account<T> {
+    private T id;
     private int sum;
 
-    public Account(Object id, int sum) {
+    public Account(T id, int sum) {
         this.id = id;
         this.sum = sum;
     }
 
-    public Object getId() {
+    public T getId() {
         return id;
     }
 
-    public void setId(Object id) {
+    public void setId(T id) {
         this.id = id;
     }
 
--- GenericTypesLeaming/GenericTypesLeaming/src/customgenerictypes/Main.java
+++ GenericTypesLeaming/GenericTypesLeaming/src/customgenerictypes/Main.java
@@ -2,13 +2,13 @@ package customgenerictypes;
 
 public class Main {
     public static void main(String[] args){
-        Account account1 = new Account(12345,500);
+        Account<Integer> account1 = new Account(12345,500);
         System.out.println(account1.getId());
 
-        Account account2 = new Account("sid2345", 5000);
+        Account<String> account2 = new Account("sid2345", 5000);
         System.out.println(account2.getId());
 
-        int a = ((Integer) account2.getId()) + 10;
+        int a = (account1.getId()) + 10; // Опасное преобразование типов
         System.out.println(a);
 
 
