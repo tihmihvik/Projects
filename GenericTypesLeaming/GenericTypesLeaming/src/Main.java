3cbc14566de5c2cbf37db3c0eb2de153716cb98e
tihmihvik <tihmihvik@gmail.com>
2022-11-17 20:13:48 +0700

    Added account class

--- GenericTypesLeaming/GenericTypesLeaming/src/Main.java
+++ GenericTypesLeaming/GenericTypesLeaming/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -19,17 +20,44 @@ public class Main {
                 System.out.println(s);
             }
         }
-        ArrayList numbers = new ArrayList(); // сохраняет Object
-        for (int i = 10; i < 10; i++){ // сохранять в коллекцию числа 10,2О..
+        ArrayList<Integer> numbers = new ArrayList(); // сохраняет Object
+        for (int i = 0; i < 10; i++){ // сохранять в коллекцию числа 10,2О..
         numbers.add(i*10);
             //numbers.add(i*10.4F); - ошибка.
 
         }
         int sum = 0;
-        for (Object o : numbers){
-            sum = sum + (Integer) o;
+        for (Integer o : numbers){
+            sum = sum + o;
         }
         System.out.println(sum);
+
+        // Generic - Основной тип<Тип параметр>
+        // Основной тип<Тип параметр1, Тип параметр2, Тип параметр3 >
+
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(7, "Привет");
+        map.put(15, "Hello");
+
+        ArrayList<String> listHello = new ArrayList<>();
+        listHello.add("Привет");
+        listHello.add("Hi");
+
+        ArrayList<String> listBye = new ArrayList<>();
+        listBye.add("Пока");
+        listBye.add("Good Bay");
+
+        ArrayList<ArrayList<String>> list = new ArrayList<>();
+        list.add(listHello);
+        list.add(listBye);
+        for (ArrayList<String> spisok : list){
+            for (String s : spisok){
+                System.out.println(s);
+            }
+
+
+        }
+
 
 
     }
---
+++ GenericTypesLeaming/GenericTypesLeaming/src/customgenerictypes/Account.java
@@ -0,0 +1,27 @@
+package customgenerictypes;
+
+public class Account {
+    private int id;
+    private int sum;
+
+    public Account(int id, int sum) {
+        this.id = id;
+        this.sum = sum;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+}
