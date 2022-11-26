11a8ec64c80a4fb8fe91b01a7bb6bb4b5332915f
tihmihvik <tihmihvik@gmail.com>
2022-11-26 15:07:26 +0700

    We have learned LinearLayout 2

--- AndroidStartProject/app/src/main/java/com/example/androidstartproject/MainActivity.java
+++ AndroidStartProject/app/src/main/java/com/example/androidstartproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidstartproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -14,8 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView textView = (TextView) findViewById(R.id.normal);
+        textView.setText("Текст из Java кода");
+        textView.setTextColor(Color.BLACK);
 
-        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+
+        /*ConstraintLayout constraintLayout = new ConstraintLayout(this);
         TextView textView = new TextView(this);
         textView.setText("Hello world!");
         textView.setTextSize(26);
@@ -28,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
         textView.setLayoutParams(layoutParams);
-        constraintLayout.addView(textView);
+        constraintLayout.addView(textView);*/
 
 //        setContentView(constraintLayout);
     }
