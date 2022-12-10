a629e9bcb8bb68e40221457f223b825bdc0531ab
tihmihvik <tihmihvik@gmail.com>
2022-12-10 18:59:04 +0700

    Explained AsyncTask class structure

--- AsyncTaskExample/app/src/main/java/ru/tihonov/asynctaskexample/AsynckTaskExample.java
+++ AsyncTaskExample/app/src/main/java/ru/tihonov/asynctaskexample/AsynckTaskExample.java
@@ -2,6 +2,7 @@ package ru.tihonov.asynctaskexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class AsynckTaskExample extends AppCompatActivity {
@@ -10,5 +11,27 @@ public class AsynckTaskExample extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asynck_task_example);
+        MyAsyncTask asyncTask = new MyAsyncTask();
+        asyncTask.execute("Hello mikhail");
+    }
+}
+
+class MyAsyncTask extends AsyncTask<String, Integer, Integer> {
+
+    @Override
+    protected void onPostExecute(Integer integer) {
+        int myProgress = 0;
+        publishProgress(myProgress);
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected Integer doInBackground(String... strings) {
+        return null;
     }
 }
