da0b1e42d3b89fdfdc9faf16b8f701e562614e0f
tihmihvik <tihmihvik@gmail.com>
2022-12-10 17:23:29 +0700

    Create AsyncTaskExample activity (создали)

---
+++ AsyncTaskExample/app/src/main/java/ru/tihonov/asynctaskexample/AsynckTaskExample.java
@@ -0,0 +1,14 @@
+package ru.tihonov.asynctaskexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AsynckTaskExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asynck_task_example);
+    }
+}
