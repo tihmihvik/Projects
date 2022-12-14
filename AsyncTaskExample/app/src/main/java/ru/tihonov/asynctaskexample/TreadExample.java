5f2de6314458aaa2ef4c73aefd6d7193d2f4cbcd
tihmihvik <tihmihvik@gmail.com>
2022-12-14 21:51:26 +0700

    Finished sparrow counter example (счётчик ворон готов)

--- AsyncTaskExample/app/src/main/java/ru/tihonov/asynctaskexample/TreadExample.java
+++ AsyncTaskExample/app/src/main/java/ru/tihonov/asynctaskexample/TreadExample.java
@@ -1,13 +1,71 @@
 package ru.tihonov.asynctaskexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class TreadExample extends AppCompatActivity {
+
+    int mCounter;
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            TextView forTextView = (TextView) findViewById(R.id.textInfo);
+            forTextView.setText("Сегодня ворон было " + mCounter++ + " штук.");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tread_example);
     }
+    /*private Runnable doInBackgrounProcessing = new Runnable() {
+        @Override
+        public void run() {
+            BackgrounThreadProcessing();
+        }
+    }
+    private void BackgrounThreadProcessing(){
+        // Трудоёмкие операции.
+    }
+
+
+    private void mainProdsing(){
+        Thread thread = new Thread(null, doInBackgrounProcessing);
+    }*/
+
+public void OnClick(View view){
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            long andTime = System.currentTimeMillis() + 20*1000;
+            while (System.currentTimeMillis() < andTime){
+                synchronized (this){
+                    try {
+                        wait(andTime - System.currentTimeMillis());
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+                Log.i("sperols", ("Сегодня ворон было " + mCounter++ + " штук."));
+                handler.sendEmptyMessage(0);
+            }
+
+
+
+        }
+    };
+
+Thread thread = new Thread(runnable);
+thread.start();
+
+}
 }
