df5d1f8de188ee80909ecd888928fa9b670e2d07
tihmihvik <tihmihvik@gmail.com>
2022-12-17 19:53:13 +0700

    Фрагмент добил, перехожу на главную

--- FragmentArticleExample/app/src/main/java/ru/tihonov/fragmentarticleexample/fragments/BlankFragment.java
+++ FragmentArticleExample/app/src/main/java/ru/tihonov/fragmentarticleexample/fragments/BlankFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import ru.tihonov.fragmentarticleexample.R;
 
@@ -16,6 +19,8 @@ import ru.tihonov.fragmentarticleexample.R;
  * create an instance of this fragment.
  */
 public class BlankFragment extends Fragment {
+    private static final int YES = 0;
+    private static final int NO = 1;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +66,35 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_blank, container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+
+
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                View radioButton = radioGroup.findViewById(checkedId);
+                int i = radioGroup.indexOfChild(radioButton);
+                TextView textView = rootView.findViewById(R.id.fragment_header);
+                switch (i) {
+                    case YES:
+                        textView.setText(R.string.yes_message);
+                        break;
+                    case NO:
+                        textView.setText(R.string.no_message);
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }
+        });
+        return  rootView;
+
     }
+
+
 }
