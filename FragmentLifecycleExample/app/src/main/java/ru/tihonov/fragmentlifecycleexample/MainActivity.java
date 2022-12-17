e1e4c0c7aa3b1fa4e5a15113b88500fa83058523
tihmihvik <tihmihvik@gmail.com>
2022-12-17 09:19:05 +0700

    Created first and second fragment

--- FragmentLifecycleExample/app/src/main/java/ru/tihonov/fragmentlifecycleexample/MainActivity.java
+++ FragmentLifecycleExample/app/src/main/java/ru/tihonov/fragmentlifecycleexample/MainActivity.java
@@ -1,7 +1,6 @@
 package ru.tihonov.fragmentlifecycleexample;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
---
+++ FragmentLifecycleExample/app/src/main/java/ru/tihonov/fragmentlifecycleexample/fragments/SecondFragment.java
@@ -0,0 +1,120 @@
+package ru.tihonov.fragmentlifecycleexample.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ru.tihonov.fragmentlifecycleexample.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SecondFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SecondFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String tagFragment1 = "FRAGMENT2";
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    //private static final String ARG_PARAM1 = "param1";
+    //private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FirstFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SecondFragment newInstance(String param1, String param2) {
+        SecondFragment fragment = new SecondFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(tagFragment1, "onCreate()");
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.d(tagFragment1, "onCreateView()");
+
+        View rootView = inflater.inflate(R.layout.fragment_first, container, false);
+        // Inflate the layout for this fragment
+        TextView tv = rootView.findViewById(R.id.tv);
+        tv.setText(tagFragment1 + " layout");
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.d(tagFragment1, "onAttach()");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(tagFragment1,"onActivity()");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(tagFragment1, "onResume()");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(tagFragment1, "onStop()");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(tagFragment1, "onDestroyView()");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(tagFragment1, "onDestroy()");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(tagFragment1, "onDetach()");
+    }
+}
