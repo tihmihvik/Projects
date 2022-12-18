99d021b8b57bf42aecaec62380ed6c4faf68c08a
tihmihvik <tihmihvik@gmail.com>
2022-12-18 20:04:54 +0700

    Вёрстку закончили

--- SharedPreferencesExample/app/src/main/java/ru/tihonov/sharedpreferencesexample/MainActivity.java
+++ SharedPreferencesExample/app/src/main/java/ru/tihonov/sharedpreferencesexample/MainActivity.java
@@ -3,8 +3,16 @@ package ru.tihonov.sharedpreferencesexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvWelcome;
+    EditText etName;
+    Button btnSubmit;
+
+    public static final String MY_PREFS_FILENAME = "hridoy.aiz.sharedpreference_my_prefs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
